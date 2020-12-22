ceffe5c5d7aad8b0a6df0eb4171874a77d0a83a7
Владислав <volodik04@mail.ru>
2020-12-22 21:04:01 +0300

    structure of code hierarchy and example of buttons

--- main.py
+++ main.py
@@ -1,14 +1,71 @@
 import pygame
 
-if __name__ == '__main__':
-    pygame.init()
-    size = WIDTH, HEIGHT = 1620, 980
-    screen = pygame.display.set_mode(size)
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        pygame.display.flip()
+pygame.init()
+size = WIDTH, HEIGHT = 600, 700
+screen = pygame.display.set_mode(size)
 
-    pygame.quit()
+
+class StartMenu:
+    def __init__(self):
+        for name in WORDS:
+            pygame.draw.rect(screen, 'grey', (200, WORDS[name], 200, 50), 0, 5)
+            font = pygame.font.Font(None, 50)
+            text = font.render(f"{name}", True, (255, 255, 255))
+            text_x = 200
+            text_y = WORDS[name]
+            screen.blit(text, (text_x + 7, text_y + 7))
+
+    def game(self, event):
+        Game().update(event)
+
+    def history(self):
+        pass
+
+    def settings(self):
+        pass
+
+    def exit_program(self):
+        exit()
+
+
+class Settings:
+    pass
+
+
+class History:
+    pass
+
+
+class Game:
+    def __init__(self):
+        pass
+
+    def update(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            if (200 <= x <= 400) and (200 <= y <= 250):
+                print('Start Game')
+            if (200 <= x <= 400) and (260 <= y <= 310):
+                print('Settings')
+            if (200 <= x <= 400) and (320 <= y <= 370):
+                print('Exit')
+
+
+WORDS = {
+    'Start Game': 200,
+    'Settings': 260,
+    'Exit': 320
+}
+
+StartMenu()
+
+running = True
+menu = StartMenu()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        menu.game(event)
+    pygame.display.flip()
+pygame.quit()
