7f23982ceeb95b9954b20fe91e591b4c0ce54ca8
Владислав <volodik04@mail.ru>
2020-12-26 19:24:29 +0300

    add: start menu

--- main.py
+++ main.py
@@ -1,71 +1,144 @@
+import os
+import sys
+
 import pygame
+import pygame_gui
+
+import history
 
 pygame.init()
-size = WIDTH, HEIGHT = 600, 700
-screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Tetris')
+SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+screen = pygame.display.set_mode(SIZE)
+clock = pygame.time.Clock()
+manager = pygame_gui.UIManager(SIZE)
+FPS = 60
 
 
-class StartMenu:
-    def __init__(self):
-        for name in WORDS:
-            pygame.draw.rect(screen, 'grey', (200, WORDS[name], 200, 50), 0, 5)
-            font = pygame.font.Font(None, 50)
-            text = font.render(f"{name}", True, (255, 255, 255))
-            text_x = 200
-            text_y = WORDS[name]
-            screen.blit(text, (text_x + 7, text_y + 7))
-
-    def game(self, event):
-        Game().update(event)
-
-    def history(self):
-        pass
-
-    def settings(self):
-        pass
-
-    def exit_program(self):
-        exit()
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
-class Settings:
-    pass
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
 
 
-class History:
-    pass
+def start_screen():
+    line = 'Tetris'
+
+    fon = pygame.transform.scale(load_image('data/startScreenBackground.jpg'), (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(fon, (0, 0))
+
+    font = pygame.font.Font(None, 100)
+    string_rendered = font.render(line, True, pygame.Color('black'))
+    intro_rect = string_rendered.get_rect()
+    intro_rect.top = 100
+    intro_rect.x = SCREEN_WIDTH // 2 - 100
+    screen.blit(string_rendered, intro_rect)
+
+    buttonWidth = 300
+    buttonHeight = 50
+    buttonTop = 250
+    buttonSpace = 20
+    buttonStartGame = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect(((SCREEN_WIDTH - buttonWidth) // 2, buttonTop), (buttonWidth, buttonHeight)),
+        text='Start Game',
+        manager=manager)
+    buttonSettings = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect(((SCREEN_WIDTH - buttonWidth) // 2, buttonTop + buttonHeight + buttonSpace),
+                                  (buttonWidth, buttonHeight)),
+        text='Settings',
+        manager=manager)
+    buttonHistory = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect(((SCREEN_WIDTH - buttonWidth) // 2, buttonTop + 2 * (buttonHeight + buttonSpace)),
+                                  (buttonWidth, buttonHeight)),
+        text='History',
+        manager=manager)
+    buttonExit = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect(((SCREEN_WIDTH - buttonWidth) // 2, buttonTop + 3 * (buttonHeight + buttonSpace)),
+                                  (buttonWidth, buttonHeight)),
+        text='Exit',
+        manager=manager)
+
+    while True:
+        time_delta = clock.tick(60) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == buttonStartGame:
+                        print('Start Game!')
+                        return
+                    if event.ui_element == buttonSettings:
+                        print('Settings!')
+                    if event.ui_element == buttonHistory:
+                        print('History!')
+
+                    if event.ui_element == buttonExit:
+                        terminate()
+
+            manager.process_events(event)
+
+        manager.update(time_delta)
+
+        pygame.display.flip()
+        manager.draw_ui(screen)
+        clock.tick(FPS)
 
 
-class Game:
-    def __init__(self):
-        pass
-
-    def update(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = event.pos
-            if (200 <= x <= 400) and (200 <= y <= 250):
-                print('Start Game')
-            if (200 <= x <= 400) and (260 <= y <= 310):
-                print('Settings')
-            if (200 <= x <= 400) and (320 <= y <= 370):
-                print('Exit')
+start_screen()
 
 
-WORDS = {
-    'Start Game': 200,
-    'Settings': 260,
-    'Exit': 320
-}
+def write_history():
+    line = 'History'
 
-StartMenu()
+    fon = pygame.transform.scale(load_image('data/startScreenBackground.jpg'), (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(fon, (0, 0))
 
-running = True
-menu = StartMenu()
+    font = pygame.font.Font(None, 100)
+    string_rendered = font.render(line, True, pygame.Color('black'))
+    intro_rect = string_rendered.get_rect()
+    intro_rect.top = 50
+    intro_rect.x = SCREEN_WIDTH // 2 - 100
+    screen.blit(string_rendered, intro_rect)
 
-while running:
+    with open('history.txt', 'r', encoding='utf-8') as hist:
+        file = [line.strip() for line in hist]
+
+    while True:
+        time_delta = clock.tick(60) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            manager.process_events(event)
+
+        manager.update(time_delta)
+
+        pygame.display.flip()
+        manager.draw_ui(screen)
+        clock.tick(FPS)
+
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-        menu.game(event)
+            terminate()
+
+    clock.tick(FPS)
     pygame.display.flip()
-pygame.quit()
